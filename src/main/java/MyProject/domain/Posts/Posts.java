1007f664e54e8e8f7b4693c36e4d7bba91648b3a
duf7317 <duf7317@naver.com>
2022-06-16 20:11:36 +0900

    .gitignore3

--- src/main/java/MyProject/domain/Posts/Posts.java
+++ src/main/java/MyProject/domain/Posts/Posts.java
@@ -1,4 +1,30 @@
 package MyProject.domain.Posts;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+    private String title;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 }
