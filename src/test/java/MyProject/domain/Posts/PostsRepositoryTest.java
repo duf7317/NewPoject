2e2b091a6bc125d6adca1a1270c49731da257596
duf7317 <duf7317@naver.com>
2022-06-18 15:51:25 +0900

    insert.mustache Mapping Test OK

--- src/test/java/MyProject/domain/Posts/PostsRepositoryTest.java
+++ src/test/java/MyProject/domain/Posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,5 +40,24 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
         assertThat(posts.getAuthor()).isEqualTo("spring@naver.com");
+    }
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022,6,18,14,20,0 );
+        postsRepository.save(Posts.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                        .build()
+                );
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+        //than
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>>> createDate=" + posts.getCreatedDate()+" , modifiedDate= "+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 }
