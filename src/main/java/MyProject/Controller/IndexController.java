28a18dd0e8cc3bb903bb8b7be4098a9e8481e431
duf7317 <duf7317@naver.com>
2022-06-19 20:56:29 +0900

    게시판 출력 및 수정

--- src/main/java/MyProject/Controller/IndexController.java
+++ src/main/java/MyProject/Controller/IndexController.java
@@ -1,17 +1,20 @@
 package MyProject.Controller;
 
 import MyProject.domain.Posts.PostsRepository;
+import MyProject.dto.PostsResponseDto;
+import MyProject.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsRepository postsRepository;
-
+    private final PostsService postsService;
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsRepository.findAllDesc());
@@ -20,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
