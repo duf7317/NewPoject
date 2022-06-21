6dbb19e458730dbb1be4fc77d6e9103b1cc99587
duf7317 <duf7317@naver.com>
2022-06-21 20:33:57 +0900

    Google OAuth2기능 구현

--- src/main/java/MyProject/Controller/IndexController.java
+++ src/main/java/MyProject/Controller/IndexController.java
@@ -1,6 +1,7 @@
 package MyProject.Controller;
 
 import MyProject.domain.Posts.PostsRepository;
+import MyProject.domain.User.SessionUser.SessionUser;
 import MyProject.dto.PostsResponseDto;
 import MyProject.service.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsRepository postsRepository;
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsRepository.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
@@ -31,4 +39,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
