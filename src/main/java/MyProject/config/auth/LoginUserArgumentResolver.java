2bf15ee9b24dc7ef010cd7421b074b437610441e
duf7317 <duf7317@naver.com>
2022-06-23 00:06:59 +0900

    ..

---
+++ src/main/java/MyProject/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,7 @@
+package MyProject.config.auth;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.handler.HandlerExceptionResolverComposite;
+
+public class LoginUserArgumentResolver implements {
+}
