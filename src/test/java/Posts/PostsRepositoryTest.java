d722bd7ea9c669177aed4ca10cd3b259f88fdd67
duf7317 <duf7317@naver.com>
2022-06-26 21:02:50 +0900

    Session jdbc

--- src/test/java/Posts/PostsRepositoryTest.java
+++ src/test/java/Posts/PostsRepositoryTest.java
@@ -1,5 +1,7 @@
-package MyProject.domain.Posts;
+package Posts;
 
+import MyProject.domain.Posts.Posts;
+import MyProject.domain.Posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
